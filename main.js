b71cecb039128db8b40738a79785d121a510f54a
Ben-Kilinski <ben.kilinski@gmail.com>
2023-09-13 15:00:26 +0300

    for of

---
+++ main.js
@@ -0,0 +1,30 @@
+//task 4
+
+function task4() {
+    let names = ["Itzhak", "Ron", "Matan", "Ben", "Maya"]
+    for (const item of names) { console.log(item) }
+}
+task4()
+
+//task 5
+function task5() {
+    let ages = [6, 92, 76, 44, 2, 22, 58, 64, 67, 112]
+    for (const item of ages) {
+        if (item >= 25) { console.log(item) }
+    }
+}
+task5()
+
+//task 6
+function task6() {
+    let FamilyNames = ["Kilinski", "Johnson", "Goldberg", "Alves", "Potter", "Parker"]
+    for (let item of FamilyNames) {
+        console.log(item.toUpperCase()) 
+    }
+}
+task6()
+
+//task 7 
+function task7(){
+    let list = document.getElementById("ul")
+}
